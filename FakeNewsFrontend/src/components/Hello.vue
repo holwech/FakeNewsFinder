43c221e887931e068095085750c8083149658a6a
Joachim Holwech <joachim.holwech@gmail.com>
2017-06-24 21:41:42 +0200

    Improvements

---
+++ FakeNewsFrontend/src/components/Hello.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+    <md-layout md-gutter>
+      <md-layout class="layout">
+        <md-whiteframe class="main">
+          <form novalidate @submit.stop.prevent="submit">
+            <md-input-container>
+              <label>Initial value</label>
+              <md-input v-model="inputUrl"></md-input>
+              <md-button class="md-raised md-primary" type="submit">Send</md-button>
+            </md-input-container>
+            </br>
+            {{ inputUrl }}
+          </form>
+        </md-whiteframe>
+      </md-layout>
+    </md-layout>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      inputUrl: '',
+      response: ''
+    }
+  },
+  methods: {
+    submit (event) {
+      const api = 'http://localhost:8000'
+      console.log('Api is ' + api)
+      this.axios.get(api).then(response => {
+        response = response.data
+        console.log('Response success!')
+      }).catch(error => {
+        console.log('AJAX FAILED: ' + error)
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.layout {
+  margin: auto;
+}
+.main {
+  height: 200px;
+  width: 400px;
+}
+</style>
--- NewsScraper.py
+++ NewsScraper.py
@@ -9,42 +9,86 @@ import json
 import newspaper
 from newspaper import Article
 from collections import namedtuple
+from time import mktime
+from datetime import datetime
 
+def datetime_handler(x):
+    if isinstance(x, datetime.datetime):
+        return x.isoformat()
+    raise TypeError("Unknown type")
 data = {}
 data['newspapers'] = {}
-
-
-# In[ ]:
-
 
 with open('NewsPapersRSS.json') as data_file:
     RSS = json.load(data_file)
 
-
-# In[ ]:
-
-
 count = 1
 for company, value in RSS.items():
-    print("Downloading articles from ", company)
-    d = fp.parse(value['rss'])
-    newsPaper = {
-        "rss": value['rss'],
-        "articles": []
-    }
-    for entry in d.entries:
-        if hasattr(entry, 'published'):
+    if 'rss' in value:
+        d = fp.parse(value['rss'])
+        print("Downloading articles from ", company)
+        newsPaper = {
+            "rss": value['rss'],
+            "link": value['link'],
+            "articles": []
+        }
+        for entry in d.entries:
+            if hasattr(entry, 'published'):
+                if count > 400:
+                    break
+                article = {}
+                article['link'] = entry.link
+                date = entry.published_parsed
+                article['published'] = datetime.fromtimestamp(mktime(date)).isoformat()
+                try:
+                    content = Article(entry.link)
+                    content.download()
+                    content.parse()
+                except Exception as e:
+                    print(e)
+                    print("continuing...")
+                    continue
+                article['title'] = content.title
+                article['text'] = content.text
+                newsPaper['articles'].append(article)
+                print(count, "articles downloaded from", company, ", url: ", entry.link)
+                count = count + 1
+    else:
+        print("Building site for ", company)
+        paper = newspaper.build(value['link'], memoize_articles=False)
+        newsPaper = {
+            "link": value['link'],
+            "articles": []
+        }
+        noneTypeCount = 0
+        for content in paper.articles:
+            if count > 400:
+                break
+            try:
+                content.download()
+                content.parse()
+            except Exception as e:
+                print(e)
+                print("continuing...")
+                continue
+            if content.publish_date is None:
+                print(count, " Article has date of type None...")
+                noneTypeCount = noneTypeCount + 1
+                if noneTypeCount > 10:
+                    print("Too many noneType dates, aborting...")
+                    noneTypeCount = 0
+                    break
+                count = count + 1
+                continue
             article = {}
-            article['link'] = entry.link
-            article['published'] = entry.published
-            content = Article(entry.link)
-            content.download()
-            content.parse()
             article['title'] = content.title
             article['text'] = content.text
+            article['link'] = content.url
+            article['published'] = content.publish_date.isoformat()
             newsPaper['articles'].append(article)
-            print(count, "articles downloaded from", company, ", url: ", entry.link)
+            print(count, "articles downloaded from", company, " using newspaper, url: ", content.url)
             count = count + 1
+            noneTypeCount = 0
     count = 1
     data['newspapers'][company] = newsPaper
 
@@ -55,27 +99,8 @@ for company, value in RSS.items():
 try:
     with open('scraped_articles_rss.json', 'w') as outfile:
         json.dump(data, outfile)
-except:
-    print('Sad :(')
+except Exception as e: print(e)
 
-
-# In[ ]:
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
